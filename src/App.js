19f8e4ecbe4bcf08dd3637fccceb5b100bb2d0e4
yeongjun Kim <ehdwjs500dnj@naver.com>
2022-11-21 03:41:57 +0900

    Movie App Update

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
  return (
   <Router>     {/*라우터를 렌더링  */}
     <Routes>   {/*라우트를 찾음 */}
-      <Route path={process.env.PUBLIC_URL +"/hello"} element={<h1>Hello</h1>}/>
-      <Route path={process.env.PUBLIC_URL +"/movie/:id"} element={<Detail/>}/> {/* url이 /movie면 Detail 라우트를 렌더링 */}
-      <Route path={process.env.PUBLIC_URL +"/"} element={<Home/>}/>         {/* url이 아무것도 없으면 Home 라우트를 렌더링 */}
+      <Route path="/hello" element={<h1>Hello</h1>}/>
+      <Route path="/movie/:id" element={<Detail/>}/> {/* url이 /movie면 Detail 라우트를 렌더링 */}
+      <Route path="/" element={<Home/>}/>         {/* url이 아무것도 없으면 Home 라우트를 렌더링 */}
     </Routes>
   </Router>
  )
